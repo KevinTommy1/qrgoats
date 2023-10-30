fdc2389d1bf191793194539028b9f1b05cc95fee
Kevin_Hamelink <240424@student.scalda.nl>
2023-10-30 11:15:55 +0100

    Setup van project

---
+++ src/main/java/nl/delphinity/qrgoats/QrgoatsApplication.java
@@ -0,0 +1,13 @@
+package nl.delphinity.qrgoats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrgoatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrgoatsApplication.class, args);
+	}
+
+}
